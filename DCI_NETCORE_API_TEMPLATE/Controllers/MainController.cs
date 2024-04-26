2fcce4c6fb0df1015a4900b69eb494a849fe99bf
github-devmaker <peerapong.kiadtipongwiriya@gmail.com>
2024-04-26 15:36:31 +0700

    update
    update

---
+++ DCI_NETCORE_API_TEMPLATE/Controllers/MainController.cs
@@ -0,0 +1,34 @@
+﻿using DCI_NETCORE_API_TEMPLATE.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DCI_NETCORE_API_TEMPLATE.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MainController : Controller
+    {
+        private OraConnectDB ORA = new OraConnectDB("ORA_DATABASE_NAME");
+        private SqlConnectDB SQL = new SqlConnectDB("SQL_DATABASE_NAME");
+
+        public MainController(OraConnectDB oRA, SqlConnectDB sQL)
+        {
+            ORA = oRA;
+            SQL = sQL;
+        }
+
+        [HttpGet]
+        [Route("/get")]
+        public IActionResult GetData(string param)
+        {
+
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("/post")]
+        public IActionResult PostData([FromBody] ModelExample param)
+        {
+            return Ok();
+        }
+    }
+}
